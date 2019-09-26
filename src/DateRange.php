b9002fb1e48d8996b7c4360b89c0959f06db47e7
ManojKiran <manoj@sflad.com>
2019-09-26 13:39:12 +0530

    support for custom field

--- src/DateRange.php
+++ src/DateRange.php
@@ -9,12 +9,13 @@ trait DateRange
     /**
      * Scope a query to only include the current minute entries.
      *
-     * @param $query
+     * @param \Illuminate\Database\Eloquent\Builder  $query
+     * @param string  $fieldName
      * @return \Illuminate\Database\Eloquent\Builder
      */
-    public function scopeCurrentMinute($query)
+    public function scopeCurrentMinute($query, string $fieldName = 'created_at')
     {
-        return $query->whereBetween('created_at', [
+        return $query->whereBetween($fieldName, [
             Carbon::now()->second(0),
             Carbon::now()
         ]);
@@ -23,12 +24,13 @@ trait DateRange
     /**
      * Scope a query to only include the last minute entries.
      *
-     * @param $query
+     * @param \Illuminate\Database\Eloquent\Builder  $query
+     * @param string  $fieldName
      * @return \Illuminate\Database\Eloquent\Builder
      */
-    public function scopelastMinute($query)
+    public function scopelastMinute($query, string $fieldName = 'created_at')
     {
-        return $query->whereBetween('created_at', [
+        return $query->whereBetween($fieldName, [
             Carbon::now()->subMinute()->second(0),
             Carbon::now()->second(0)
         ]);
@@ -37,12 +39,13 @@ trait DateRange
     /**
      * Scope a query to only include the current hour entries.
      *
-     * @param $query
+     * @param \Illuminate\Database\Eloquent\Builder  $query
+     * @param string  $fieldName
      * @return \Illuminate\Database\Eloquent\Builder
      */
-    public function scopeCurrentHour($query)
+    public function scopeCurrentHour($query, string $fieldName = 'created_at')
     {
-        return $query->whereBetween('created_at', [
+        return $query->whereBetween($fieldName, [
             Carbon::now()->minute(0)->second(0),
             Carbon::now()
         ]);
@@ -51,12 +54,13 @@ trait DateRange
     /**
      * Scope a query to only include the last hour entries.
      *
-     * @param $query
+     * @param \Illuminate\Database\Eloquent\Builder  $query
+     * @param  string $fieldName
      * @return \Illuminate\Database\Eloquent\Builder
      */
-    public function scopeLastHour($query)
+    public function scopeLastHour($query, string $fieldName = 'created_at')
     {
-        return $query->whereBetween('created_at', [
+        return $query->whereBetween($fieldName, [
             Carbon::now()->subHour()->minute(0)->second(0),
             Carbon::now()->minute(0)->second(0)
         ]);
@@ -65,12 +69,13 @@ trait DateRange
     /**
      * Scope a query to only include the current day entries.
      *
-     * @param $query
+     * @param \Illuminate\Database\Eloquent\Builder  $query
+     * @param  string $fieldName
      * @return \Illuminate\Database\Eloquent\Builder
      */
-    public function scopeCurrentDay($query)
+    public function scopeCurrentDay($query, string $fieldName = 'created_at')
     {
-        return $query->whereBetween('created_at', [
+        return $query->whereBetween($fieldName, [
             Carbon::now()->startOfDay(),
             Carbon::now()
         ]);
@@ -79,12 +84,13 @@ trait DateRange
     /**
      * Scope a query to only include the last day entries.
      *
-     * @param $query
+     * @param \Illuminate\Database\Eloquent\Builder  $query
+     * @param  string $fieldName
      * @return \Illuminate\Database\Eloquent\Builder
      */
-    public function scopeLastDay($query)
+    public function scopeLastDay($query, string $fieldName = 'created_at')
     {
-        return $query->whereBetween('created_at', [
+        return $query->whereBetween($fieldName, [
             Carbon::now()->subDay()->startOfDay(),
             Carbon::now()->startOfDay()
         ]);
@@ -93,12 +99,13 @@ trait DateRange
     /**
      * Scope a query to only include the current week entries.
      *
-     * @param $query
+     * @param \Illuminate\Database\Eloquent\Builder  $query
+     * @param  string $fieldName
      * @return \Illuminate\Database\Eloquent\Builder
      */
-    public function scopeCurrentWeek($query)
+    public function scopeCurrentWeek($query, string $fieldName = 'created_at')
     {
-        return $query->whereBetween('created_at', [
+        return $query->whereBetween($fieldName, [
             Carbon::now()->startOfWeek(),
             Carbon::now()
         ]);
@@ -107,12 +114,13 @@ trait DateRange
     /**
      * Scope a query to only include the last week entries.
      *
-     * @param $query
+     * @param \Illuminate\Database\Eloquent\Builder  $query
+     * @param  string $fieldName
      * @return \Illuminate\Database\Eloquent\Builder
      */
-    public function scopeLastWeek($query)
+    public function scopeLastWeek($query, string $fieldName = 'created_at')
     {
-        return $query->whereBetween('created_at', [
+        return $query->whereBetween($fieldName, [
             Carbon::now()->subWeek()->startOfWeek(),
             Carbon::now()->subWeek()->endOfWeek()
         ]);
@@ -121,12 +129,13 @@ trait DateRange
     /**
      * Scope a query to only include the current month entries.
      *
-     * @param $query
+     * @param \Illuminate\Database\Eloquent\Builder  $query
+     * @param  string $fieldName
      * @return \Illuminate\Database\Eloquent\Builder
      */
-    public function scopeCurrentMonth($query)
+    public function scopeCurrentMonth($query, string $fieldName = 'created_at')
     {
-        return $query->whereBetween('created_at', [
+        return $query->whereBetween($fieldName, [
             Carbon::now()->startOfMonth(),
             Carbon::now()
         ]);
@@ -135,12 +144,13 @@ trait DateRange
     /**
      * Scope a query to only include the last month entries.
      *
-     * @param $query
+     * @param \Illuminate\Database\Eloquent\Builder  $query
+     * @param  string $fieldName
      * @return \Illuminate\Database\Eloquent\Builder
      */
-    public function scopeLastMonth($query)
+    public function scopeLastMonth($query, string $fieldName = 'created_at')
     {
-        return $query->whereBetween('created_at', [
+        return $query->whereBetween($fieldName, [
             Carbon::now()->subMonth()->startOfMonth(),
             Carbon::now()->subMonth()->endOfMonth()
         ]);
@@ -149,12 +159,13 @@ trait DateRange
     /**
      * Scope a query to only include the current year entries.
      *
-     * @param $query
+     * @param \Illuminate\Database\Eloquent\Builder  $query
+     * @param  string $fieldName
      * @return \Illuminate\Database\Eloquent\Builder
      */
-    public function scopeCurrentYear($query)
+    public function scopeCurrentYear($query, string $fieldName = 'created_at')
     {
-        return $query->whereBetween('created_at', [
+        return $query->whereBetween($fieldName, [
             Carbon::now()->startOfYear(),
             Carbon::now()
         ]);
@@ -163,12 +174,13 @@ trait DateRange
     /**
      * Scope a query to only include the last year entries.
      *
-     * @param $query
+     * @param \Illuminate\Database\Eloquent\Builder  $query
+     * @param  string $fieldName
      * @return \Illuminate\Database\Eloquent\Builder
      */
-    public function scopeLastYear($query)
+    public function scopeLastYear($query, string $fieldName = 'created_at')
     {
-        return $query->whereBetween('created_at', [
+        return $query->whereBetween($fieldName, [
             Carbon::now()->subYear()->startOfYear(),
             Carbon::now()->subYear()->endOfYear()
         ]);
@@ -177,13 +189,14 @@ trait DateRange
     /**
      * Scope a query to only include the last x seconds entries.
      *
-     * @param $query
+     * @param \Illuminate\Database\Eloquent\Builder  $query
      * @param int $countSeconds
+     * @param string  $fieldName
      * @return \Illuminate\Database\Eloquent\Builder
      */
-    public function scopeLastSeconds($query, int $countSeconds)
+    public function scopeLastSeconds($query, int $countSeconds, string $fieldName = 'created_at')
     {
-        return $query->whereBetween('created_at', [
+        return $query->whereBetween($fieldName, [
             Carbon::now()->subSeconds($countSeconds),
             Carbon::now()
         ]);
@@ -192,13 +205,14 @@ trait DateRange
     /**
      * Scope a query to only include the last x minutes entries.
      *
-     * @param $query
+     * @param \Illuminate\Database\Eloquent\Builder  $query
      * @param int $countMinutes
+     * @param string  $fieldName
      * @return \Illuminate\Database\Eloquent\Builder
      */
-    public function scopeLastMinutes($query, int $countMinutes)
+    public function scopeLastMinutes($query, int $countMinutes, string $fieldName = 'created_at')
     {
-        return $query->whereBetween('created_at', [
+        return $query->whereBetween($fieldName, [
             Carbon::now()->subMinutes($countMinutes)->second(0),
             Carbon::now()
         ]);
@@ -207,13 +221,14 @@ trait DateRange
     /**
      * Scope a query to only include the last x hours entries.
      *
-     * @param $query
+     * @param \Illuminate\Database\Eloquent\Builder  $query
      * @param int $countHours
+     * @param string  $fieldName
      * @return \Illuminate\Database\Eloquent\Builder
      */
-    public function scopeLastHours($query, int $countHours)
+    public function scopeLastHours($query, int $countHours, string $fieldName = 'created_at')
     {
-        return $query->whereBetween('created_at', [
+        return $query->whereBetween($fieldName, [
             Carbon::now()->subHours($countHours)->minute(0),
             Carbon::now()
         ]);
@@ -222,13 +237,14 @@ trait DateRange
     /**
      * Scope a query to only include the last x days entries.
      *
-     * @param $query
+     * @param \Illuminate\Database\Eloquent\Builder  $query
      * @param int $countDays
+     * @param string  $fieldName
      * @return \Illuminate\Database\Eloquent\Builder
      */
-    public function scopeLastDays($query, int $countDays)
+    public function scopeLastDays($query, int $countDays, string $fieldName = 'created_at')
     {
-        return $query->whereBetween('created_at', [
+        return $query->whereBetween($fieldName, [
             Carbon::now()->subDays($countDays)->startOfDay(),
             Carbon::now()
         ]);
@@ -237,13 +253,14 @@ trait DateRange
     /**
      * Scope a query to only include the last x weeks entries.
      *
-     * @param $query
+     * @param \Illuminate\Database\Eloquent\Builder  $query
      * @param int $countWeeks
+     * @param string  $fieldName
      * @return \Illuminate\Database\Eloquent\Builder
      */
-    public function scopeLastWeeks($query, int $countWeeks)
+    public function scopeLastWeeks($query, int $countWeeks, string $fieldName = 'created_at')
     {
-        return $query->whereBetween('created_at', [
+        return $query->whereBetween($fieldName, [
             Carbon::now()->subWeeks($countWeeks)->startOfWeek(),
             Carbon::now()
         ]);
@@ -252,13 +269,14 @@ trait DateRange
     /**
      * Scope a query to only include the last x months entries.
      *
-     * @param $query
+     * @param \Illuminate\Database\Eloquent\Builder  $query
      * @param int $countMonths
+     * @param string  $fieldName
      * @return \Illuminate\Database\Eloquent\Builder
      */
-    public function scopeLastMonths($query, int $countMonths)
+    public function scopeLastMonths($query, int $countMonths, string $fieldName = 'created_at')
     {
-        return $query->whereBetween('created_at', [
+        return $query->whereBetween($fieldName, [
             Carbon::now()->subMonths($countMonths)->startOfMonth(),
             Carbon::now()
         ]);
@@ -267,13 +285,14 @@ trait DateRange
     /**
      * Scope a query to only include the last x years entries.
      *
-     * @param $query
+     * @param \Illuminate\Database\Eloquent\Builder  $query
      * @param int $countYears
+     * @param string  $fieldName
      * @return \Illuminate\Database\Eloquent\Builder
      */
-    public function scopeLastYears($query, int $countYears)
+    public function scopeLastYears($query, int $countYears, string $fieldName = 'created_at')
     {
-        return $query->whereBetween('created_at', [
+        return $query->whereBetween($fieldName, [
             Carbon::now()->subYears($countYears)->startOfYear(),
             Carbon::now()
         ]);
