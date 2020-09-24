1da32be715ca3c2ff985080fcaa5770f6e87231f
hedii <contact@hedichaibi.com>
2020-09-24 11:23:42 +0200

    Laravel 7 support
    Related to #5

--- src/DateRange.php
+++ src/DateRange.php
@@ -3,17 +3,18 @@
 namespace Hedii\LaravelDateRange;
 
 use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Builder;
 
 trait DateRange
 {
     /**
      * Scope a query to only include the current minute entries.
      *
-     * @param \Illuminate\Database\Eloquent\Builder  $query
-     * @param string  $fieldName
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @param string $fieldName
      * @return \Illuminate\Database\Eloquent\Builder
      */
-    public function scopeCurrentMinute($query, string $fieldName = 'created_at')
+    public function scopeCurrentMinute(Builder $query, string $fieldName = 'created_at'): Builder
     {
         return $query->whereBetween($fieldName, [
             Carbon::now()->second(0),
@@ -24,11 +25,11 @@ trait DateRange
     /**
      * Scope a query to only include the last minute entries.
      *
-     * @param \Illuminate\Database\Eloquent\Builder  $query
-     * @param string  $fieldName
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @param string $fieldName
      * @return \Illuminate\Database\Eloquent\Builder
      */
-    public function scopelastMinute($query, string $fieldName = 'created_at')
+    public function scopeLastMinute(Builder $query, string $fieldName = 'created_at'): Builder
     {
         return $query->whereBetween($fieldName, [
             Carbon::now()->subMinute()->second(0),
@@ -39,11 +40,11 @@ trait DateRange
     /**
      * Scope a query to only include the current hour entries.
      *
-     * @param \Illuminate\Database\Eloquent\Builder  $query
-     * @param string  $fieldName
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @param string $fieldName
      * @return \Illuminate\Database\Eloquent\Builder
      */
-    public function scopeCurrentHour($query, string $fieldName = 'created_at')
+    public function scopeCurrentHour(Builder $query, string $fieldName = 'created_at'): Builder
     {
         return $query->whereBetween($fieldName, [
             Carbon::now()->minute(0)->second(0),
@@ -54,11 +55,11 @@ trait DateRange
     /**
      * Scope a query to only include the last hour entries.
      *
-     * @param \Illuminate\Database\Eloquent\Builder  $query
-     * @param  string $fieldName
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @param string $fieldName
      * @return \Illuminate\Database\Eloquent\Builder
      */
-    public function scopeLastHour($query, string $fieldName = 'created_at')
+    public function scopeLastHour(Builder $query, string $fieldName = 'created_at'): Builder
     {
         return $query->whereBetween($fieldName, [
             Carbon::now()->subHour()->minute(0)->second(0),
@@ -69,11 +70,11 @@ trait DateRange
     /**
      * Scope a query to only include the current day entries.
      *
-     * @param \Illuminate\Database\Eloquent\Builder  $query
-     * @param  string $fieldName
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @param string $fieldName
      * @return \Illuminate\Database\Eloquent\Builder
      */
-    public function scopeCurrentDay($query, string $fieldName = 'created_at')
+    public function scopeCurrentDay(Builder $query, string $fieldName = 'created_at'): Builder
     {
         return $query->whereBetween($fieldName, [
             Carbon::now()->startOfDay(),
@@ -84,11 +85,11 @@ trait DateRange
     /**
      * Scope a query to only include the last day entries.
      *
-     * @param \Illuminate\Database\Eloquent\Builder  $query
-     * @param  string $fieldName
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @param string $fieldName
      * @return \Illuminate\Database\Eloquent\Builder
      */
-    public function scopeLastDay($query, string $fieldName = 'created_at')
+    public function scopeLastDay(Builder $query, string $fieldName = 'created_at'): Builder
     {
         return $query->whereBetween($fieldName, [
             Carbon::now()->subDay()->startOfDay(),
@@ -99,11 +100,11 @@ trait DateRange
     /**
      * Scope a query to only include the current week entries.
      *
-     * @param \Illuminate\Database\Eloquent\Builder  $query
-     * @param  string $fieldName
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @param string $fieldName
      * @return \Illuminate\Database\Eloquent\Builder
      */
-    public function scopeCurrentWeek($query, string $fieldName = 'created_at')
+    public function scopeCurrentWeek(Builder $query, string $fieldName = 'created_at'): Builder
     {
         return $query->whereBetween($fieldName, [
             Carbon::now()->startOfWeek(),
@@ -114,11 +115,11 @@ trait DateRange
     /**
      * Scope a query to only include the last week entries.
      *
-     * @param \Illuminate\Database\Eloquent\Builder  $query
-     * @param  string $fieldName
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @param string $fieldName
      * @return \Illuminate\Database\Eloquent\Builder
      */
-    public function scopeLastWeek($query, string $fieldName = 'created_at')
+    public function scopeLastWeek(Builder $query, string $fieldName = 'created_at'): Builder
     {
         return $query->whereBetween($fieldName, [
             Carbon::now()->subWeek()->startOfWeek(),
@@ -129,11 +130,11 @@ trait DateRange
     /**
      * Scope a query to only include the current month entries.
      *
-     * @param \Illuminate\Database\Eloquent\Builder  $query
-     * @param  string $fieldName
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @param string $fieldName
      * @return \Illuminate\Database\Eloquent\Builder
      */
-    public function scopeCurrentMonth($query, string $fieldName = 'created_at')
+    public function scopeCurrentMonth(Builder $query, string $fieldName = 'created_at'): Builder
     {
         return $query->whereBetween($fieldName, [
             Carbon::now()->startOfMonth(),
@@ -144,11 +145,11 @@ trait DateRange
     /**
      * Scope a query to only include the last month entries.
      *
-     * @param \Illuminate\Database\Eloquent\Builder  $query
-     * @param  string $fieldName
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @param string $fieldName
      * @return \Illuminate\Database\Eloquent\Builder
      */
-    public function scopeLastMonth($query, string $fieldName = 'created_at')
+    public function scopeLastMonth(Builder $query, string $fieldName = 'created_at'): Builder
     {
         return $query->whereBetween($fieldName, [
             Carbon::now()->subMonth()->startOfMonth(),
@@ -159,11 +160,11 @@ trait DateRange
     /**
      * Scope a query to only include the current year entries.
      *
-     * @param \Illuminate\Database\Eloquent\Builder  $query
-     * @param  string $fieldName
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @param string $fieldName
      * @return \Illuminate\Database\Eloquent\Builder
      */
-    public function scopeCurrentYear($query, string $fieldName = 'created_at')
+    public function scopeCurrentYear(Builder $query, string $fieldName = 'created_at'): Builder
     {
         return $query->whereBetween($fieldName, [
             Carbon::now()->startOfYear(),
@@ -174,11 +175,11 @@ trait DateRange
     /**
      * Scope a query to only include the last year entries.
      *
-     * @param \Illuminate\Database\Eloquent\Builder  $query
-     * @param  string $fieldName
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @param string $fieldName
      * @return \Illuminate\Database\Eloquent\Builder
      */
-    public function scopeLastYear($query, string $fieldName = 'created_at')
+    public function scopeLastYear(Builder $query, string $fieldName = 'created_at'): Builder
     {
         return $query->whereBetween($fieldName, [
             Carbon::now()->subYear()->startOfYear(),
@@ -189,12 +190,12 @@ trait DateRange
     /**
      * Scope a query to only include the last x seconds entries.
      *
-     * @param \Illuminate\Database\Eloquent\Builder  $query
+     * @param \Illuminate\Database\Eloquent\Builder $query
      * @param int $countSeconds
-     * @param string  $fieldName
+     * @param string $fieldName
      * @return \Illuminate\Database\Eloquent\Builder
      */
-    public function scopeLastSeconds($query, int $countSeconds, string $fieldName = 'created_at')
+    public function scopeLastSeconds(Builder $query, int $countSeconds, string $fieldName = 'created_at'): Builder
     {
         return $query->whereBetween($fieldName, [
             Carbon::now()->subSeconds($countSeconds),
@@ -205,12 +206,12 @@ trait DateRange
     /**
      * Scope a query to only include the last x minutes entries.
      *
-     * @param \Illuminate\Database\Eloquent\Builder  $query
+     * @param \Illuminate\Database\Eloquent\Builder $query
      * @param int $countMinutes
-     * @param string  $fieldName
+     * @param string $fieldName
      * @return \Illuminate\Database\Eloquent\Builder
      */
-    public function scopeLastMinutes($query, int $countMinutes, string $fieldName = 'created_at')
+    public function scopeLastMinutes(Builder $query, int $countMinutes, string $fieldName = 'created_at'): Builder
     {
         return $query->whereBetween($fieldName, [
             Carbon::now()->subMinutes($countMinutes)->second(0),
@@ -221,12 +222,12 @@ trait DateRange
     /**
      * Scope a query to only include the last x hours entries.
      *
-     * @param \Illuminate\Database\Eloquent\Builder  $query
+     * @param \Illuminate\Database\Eloquent\Builder $query
      * @param int $countHours
-     * @param string  $fieldName
+     * @param string $fieldName
      * @return \Illuminate\Database\Eloquent\Builder
      */
-    public function scopeLastHours($query, int $countHours, string $fieldName = 'created_at')
+    public function scopeLastHours(Builder $query, int $countHours, string $fieldName = 'created_at'): Builder
     {
         return $query->whereBetween($fieldName, [
             Carbon::now()->subHours($countHours)->minute(0),
@@ -237,12 +238,12 @@ trait DateRange
     /**
      * Scope a query to only include the last x days entries.
      *
-     * @param \Illuminate\Database\Eloquent\Builder  $query
+     * @param \Illuminate\Database\Eloquent\Builder $query
      * @param int $countDays
-     * @param string  $fieldName
+     * @param string $fieldName
      * @return \Illuminate\Database\Eloquent\Builder
      */
-    public function scopeLastDays($query, int $countDays, string $fieldName = 'created_at')
+    public function scopeLastDays(Builder $query, int $countDays, string $fieldName = 'created_at'): Builder
     {
         return $query->whereBetween($fieldName, [
             Carbon::now()->subDays($countDays)->startOfDay(),
@@ -253,12 +254,12 @@ trait DateRange
     /**
      * Scope a query to only include the last x weeks entries.
      *
-     * @param \Illuminate\Database\Eloquent\Builder  $query
+     * @param \Illuminate\Database\Eloquent\Builder $query
      * @param int $countWeeks
-     * @param string  $fieldName
+     * @param string $fieldName
      * @return \Illuminate\Database\Eloquent\Builder
      */
-    public function scopeLastWeeks($query, int $countWeeks, string $fieldName = 'created_at')
+    public function scopeLastWeeks(Builder $query, int $countWeeks, string $fieldName = 'created_at'): Builder
     {
         return $query->whereBetween($fieldName, [
             Carbon::now()->subWeeks($countWeeks)->startOfWeek(),
@@ -269,12 +270,12 @@ trait DateRange
     /**
      * Scope a query to only include the last x months entries.
      *
-     * @param \Illuminate\Database\Eloquent\Builder  $query
+     * @param \Illuminate\Database\Eloquent\Builder $query
      * @param int $countMonths
-     * @param string  $fieldName
+     * @param string $fieldName
      * @return \Illuminate\Database\Eloquent\Builder
      */
-    public function scopeLastMonths($query, int $countMonths, string $fieldName = 'created_at')
+    public function scopeLastMonths(Builder $query, int $countMonths, string $fieldName = 'created_at'): Builder
     {
         return $query->whereBetween($fieldName, [
             Carbon::now()->subMonths($countMonths)->startOfMonth(),
@@ -285,12 +286,12 @@ trait DateRange
     /**
      * Scope a query to only include the last x years entries.
      *
-     * @param \Illuminate\Database\Eloquent\Builder  $query
+     * @param \Illuminate\Database\Eloquent\Builder $query
      * @param int $countYears
-     * @param string  $fieldName
+     * @param string $fieldName
      * @return \Illuminate\Database\Eloquent\Builder
      */
-    public function scopeLastYears($query, int $countYears, string $fieldName = 'created_at')
+    public function scopeLastYears(Builder $query, int $countYears, string $fieldName = 'created_at'): Builder
     {
         return $query->whereBetween($fieldName, [
             Carbon::now()->subYears($countYears)->startOfYear(),
--- tests/DateRangeCustomFieldTest.php
+++ tests/DateRangeCustomFieldTest.php
@@ -6,7 +6,7 @@ use Carbon\Carbon;
 
 class DateRangeCustomFieldTest extends TestCase
 {
-    public function testCurrentMinute()
+    public function testCurrentMinute(): void
     {
         $model1 = TestModel::create(['logged_in_at' => Carbon::now()->subSeconds(61)]);
         $model2 = TestModel::create(['logged_in_at' => Carbon::now()]);
@@ -18,7 +18,7 @@ class DateRangeCustomFieldTest extends TestCase
         $this->assertEquals($model2->id, $result->first()->id);
     }
 
-    public function testLastMinute()
+    public function testLastMinute(): void
     {
         $model1 = TestModel::create(['logged_in_at' => Carbon::now()->subSeconds(61)]);
         $model2 = TestModel::create(['logged_in_at' => Carbon::now()]);
@@ -30,7 +30,7 @@ class DateRangeCustomFieldTest extends TestCase
         $this->assertEquals($model1->id, $result->first()->id);
     }
 
-    public function testCurrentHour()
+    public function testCurrentHour(): void
     {
         $model1 = TestModel::create(['logged_in_at' => Carbon::now()->subHours(2)]);
         $model2 = TestModel::create(['logged_in_at' => Carbon::now()]);
@@ -42,7 +42,7 @@ class DateRangeCustomFieldTest extends TestCase
         $this->assertEquals($model2->id, $result->first()->id);
     }
 
-    public function testLastHour()
+    public function testLastHour(): void
     {
         $model1 = TestModel::create(['logged_in_at' => Carbon::now()->subHours(2)]);
         $model2 = TestModel::create(['logged_in_at' => Carbon::now()->minute(0)->subMinutes(3)]);
@@ -54,7 +54,7 @@ class DateRangeCustomFieldTest extends TestCase
         $this->assertEquals($model2->id, $result->first()->id);
     }
 
-    public function testCurrentDay()
+    public function testCurrentDay(): void
     {
         $model1 = TestModel::create(['logged_in_at' => Carbon::now()]);
         $model2 = TestModel::create(['logged_in_at' => Carbon::yesterday()]);
@@ -64,7 +64,7 @@ class DateRangeCustomFieldTest extends TestCase
         $this->assertEquals($model1->id, $result->first()->id);
     }
 
-    public function testLastDay()
+    public function testLastDay(): void
     {
         $model1 = TestModel::create(['logged_in_at' => Carbon::now()]);
         $model2 = TestModel::create(['logged_in_at' => Carbon::yesterday()]);
@@ -74,7 +74,7 @@ class DateRangeCustomFieldTest extends TestCase
         $this->assertEquals($model2->id, $result->first()->id);
     }
 
-    public function testCurrentWeek()
+    public function testCurrentWeek(): void
     {
         $model1 = TestModel::create(['logged_in_at' => Carbon::now()]);
         $model2 = TestModel::create(['logged_in_at' => Carbon::now()->subWeek()]);
@@ -86,7 +86,7 @@ class DateRangeCustomFieldTest extends TestCase
         $this->assertEquals($model1->id, $result->first()->id);
     }
 
-    public function testLastWeek()
+    public function testLastWeek(): void
     {
         $model1 = TestModel::create(['logged_in_at' => Carbon::now()]);
         $model2 = TestModel::create(['logged_in_at' => Carbon::now()->subWeek()]);
@@ -98,7 +98,7 @@ class DateRangeCustomFieldTest extends TestCase
         $this->assertEquals($model2->id, $result->first()->id);
     }
 
-    public function testCurrentMonth()
+    public function testCurrentMonth(): void
     {
         $model1 = TestModel::create(['logged_in_at' => Carbon::now()]);
         $model2 = TestModel::create(['logged_in_at' => Carbon::now()->subMonth()]);
@@ -110,7 +110,7 @@ class DateRangeCustomFieldTest extends TestCase
         $this->assertEquals($model1->id, $result->first()->id);
     }
 
-    public function testLastMonth()
+    public function testLastMonth(): void
     {
         $model1 = TestModel::create(['logged_in_at' => Carbon::now()]);
         $model2 = TestModel::create(['logged_in_at' => Carbon::now()->subMonth()]);
@@ -122,7 +122,7 @@ class DateRangeCustomFieldTest extends TestCase
         $this->assertEquals($model2->id, $result->first()->id);
     }
 
-    public function testCurrentYear()
+    public function testCurrentYear(): void
     {
         $model1 = TestModel::create(['logged_in_at' => Carbon::now()]);
         $model2 = TestModel::create(['logged_in_at' => Carbon::now()->subYear()]);
@@ -134,7 +134,7 @@ class DateRangeCustomFieldTest extends TestCase
         $this->assertEquals($model1->id, $result->first()->id);
     }
 
-    public function testLastYear()
+    public function testLastYear(): void
     {
         $model1 = TestModel::create(['logged_in_at' => Carbon::now()]);
         $model2 = TestModel::create(['logged_in_at' => Carbon::now()->subYear()]);
@@ -146,85 +146,85 @@ class DateRangeCustomFieldTest extends TestCase
         $this->assertEquals($model2->id, $result->first()->id);
     }
 
-    public function testLastSeconds()
+    public function testLastSeconds(): void
     {
         $model1 = TestModel::create(['logged_in_at' => Carbon::now()->subMinutes(6)]);
         $model2 = TestModel::create(['logged_in_at' => Carbon::now()->subSeconds(36)]);
         $model3 = TestModel::create(['logged_in_at' => Carbon::now()->subSeconds(78)]);
 
-        $result = TestModel::lastSeconds(40,'logged_in_at')->get();
+        $result = TestModel::lastSeconds(40, 'logged_in_at')->get();
 
         $this->assertEquals(1, $result->count());
         $this->assertEquals($model2->id, $result->first()->id);
     }
 
-    public function testLastMinutes()
+    public function testLastMinutes(): void
     {
         $model1 = TestModel::create(['logged_in_at' => Carbon::now()->subMinutes(6)]);
         $model2 = TestModel::create(['logged_in_at' => Carbon::now()->subMinutes(2)]);
         $model3 = TestModel::create(['logged_in_at' => Carbon::now()->subDay()]);
 
-        $result = TestModel::lastMinutes(4,'logged_in_at')->get();
+        $result = TestModel::lastMinutes(4, 'logged_in_at')->get();
 
         $this->assertEquals(1, $result->count());
         $this->assertEquals($model2->id, $result->first()->id);
     }
 
-    public function testLastHours()
+    public function testLastHours(): void
     {
         $model1 = TestModel::create(['logged_in_at' => Carbon::now()->subHours(6)]);
         $model2 = TestModel::create(['logged_in_at' => Carbon::now()->subMinutes(30)]);
         $model3 = TestModel::create(['logged_in_at' => Carbon::now()->subDay()]);
 
-        $result = TestModel::lastHours(4,'logged_in_at')->get();
+        $result = TestModel::lastHours(4, 'logged_in_at')->get();
 
         $this->assertEquals(1, $result->count());
         $this->assertEquals($model2->id, $result->first()->id);
     }
 
-    public function testLastDays()
+    public function testLastDays(): void
     {
         $model1 = TestModel::create(['logged_in_at' => Carbon::now()->subDays(19)]);
         $model2 = TestModel::create(['logged_in_at' => Carbon::now()->subDays(4)]);
         $model3 = TestModel::create(['logged_in_at' => Carbon::now()->subDays(8)]);
 
-        $result = TestModel::lastDays(4,'logged_in_at')->get();
+        $result = TestModel::lastDays(4, 'logged_in_at')->get();
 
         $this->assertEquals(1, $result->count());
         $this->assertEquals($model2->id, $result->first()->id);
     }
 
-    public function testLastWeeks()
+    public function testLastWeeks(): void
     {
         $model1 = TestModel::create(['logged_in_at' => Carbon::now()->subWeeks(3)]);
         $model2 = TestModel::create(['logged_in_at' => Carbon::now()->subDays(1)]);
         $model3 = TestModel::create(['logged_in_at' => Carbon::now()->startOfWeek()->subDays(15)]);
 
-        $result = TestModel::lastWeeks(2,'logged_in_at')->get();
+        $result = TestModel::lastWeeks(2, 'logged_in_at')->get();
 
         $this->assertEquals(1, $result->count());
         $this->assertEquals($model2->id, $result->first()->id);
     }
 
-    public function testLastMonths()
+    public function testLastMonths(): void
     {
         $model1 = TestModel::create(['logged_in_at' => Carbon::now()->subMonths(3)]);
         $model2 = TestModel::create(['logged_in_at' => Carbon::now()->subMonths(1)]);
         $model3 = TestModel::create(['logged_in_at' => Carbon::now()->startOfMonth()->subDays(70)]);
 
-        $result = TestModel::lastMonths(2,'logged_in_at')->get();
+        $result = TestModel::lastMonths(2, 'logged_in_at')->get();
 
         $this->assertEquals(1, $result->count());
         $this->assertEquals($model2->id, $result->first()->id);
     }
 
-    public function testLastYears()
+    public function testLastYears(): void
     {
         $model1 = TestModel::create(['logged_in_at' => Carbon::now()->subYears(3)]);
         $model2 = TestModel::create(['logged_in_at' => Carbon::now()->subMonths(100)]);
         $model3 = TestModel::create(['logged_in_at' => Carbon::now()->startOfYear()->subDays(70)]);
 
-        $result = TestModel::lastYears(2,'logged_in_at')->get();
+        $result = TestModel::lastYears(2, 'logged_in_at')->get();
 
         $this->assertEquals(1, $result->count());
         $this->assertEquals($model3->id, $result->first()->id);
